f38853b9a979e1a0b5cc265d011859b6cb7cc205
LaurenceLungo <pnng@connect.ust.hk>
2020-02-28 20:23:03 -0500

    refactor

--- src/main/java/com/cs3300/locationsearch/controllers/LoginController.java
+++ src/main/java/com/cs3300/locationsearch/controllers/LoginController.java
@@ -1,11 +1,10 @@
 package com.cs3300.locationsearch.controllers;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cs3300.locationsearch.model.Account;
 import com.cs3300.locationsearch.services.AccountService;
@@ -13,22 +12,32 @@ import com.cs3300.locationsearch.services.AccountService;
 
 @Controller
 public class LoginController {
-	
-	
-	
-	@RequestMapping("/submitLoginInfo/")
-	public boolean loginUser(@RequestParam(required = true) String username, 
-			@RequestParam(required = true) String pass) {
-		
-		Account account = new Account();
-		account.setEmail("Not needed for login");
-		account.setPassword(pass);
-		account.setUsername(username);
-		
-		AccountService serv = new AccountService();
-		boolean logged = serv.checkUser(account);
-		return logged;
-		
-	}
+    
+    @Autowired
+    AccountService accService;
+    
+    /**
+     * Microservice that gives status of user account in database
+     * 
+     * endpoint format: /submitLoginInfo?username={}&password={}
+     * 
+     * @param username
+     * @param password
+     * @return true if user is in database and can be logged in. False otherwise
+     */
+    @PostMapping(value="/submitLoginInfo")
+    @ResponseBody
+    public boolean loginUser(@RequestParam(value="username", required = true) String username, 
+            @RequestParam(value="password", required = true) String password) {
+        
+        Account account = new Account();
+        account.setEmail("Not needed for login");
+        account.setPassword(password);
+        account.setUsername(username);
+        
+        boolean logged = accService.checkUser(account);
+        return logged;
+        
+    }
 
 }
