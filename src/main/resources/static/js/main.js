193ba2fd7f88cbe5eabcafcfea465d5b96a7293b
Steven Nguyen <stevennguyen430@gmail.com>
2020-02-29 02:24:16 -0500

    info boxes (todo done)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,52 @@
 let map;
+let markers;
+let places;
+let openinfowindow;
 
 // init map with arbitrary values
 function initMap() {
   let uluru = {lat: -25.344, lng: 131.036};
   map = new google.maps.Map(
       document.getElementById('maprectangle'), {zoom: 4, center: uluru});
+}
+
+function openMarker(id) {
+	for (var i = 0; i < places.length; i++) {
+		if (places[i].id == id) {
+			google.maps.event.trigger(markers[i], 'click');
+			currentOpenMarker = markers[i];
+			return;
+		}
+	}
+}
+
+//add results to list group
+function addResultToList(result) {
+	let resultlist = document.getElementById("resultslist");
+	resultlist.innerHTML +=
+		'<button onclick="openMarker(this.id)" class="list-group-item" id="' + result.id + '">' + 
+		result.name + '</button>';
+}
+
+function createMarker(place) {
+	var marker = new google.maps.Marker({
+		map: map,
+		title: place.name,
+		position: place.geometry.location
+	});
+	setMarkerListener(place, marker);
+	return marker;
+}
+
+function setMarkerListener(place, marker) {
+	let infowindow = new google.maps.InfoWindow();
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent('<div><strong>' + place.name + '<strong><br>' +
+				place.formatted_address + '</div>');
+		if (openinfowindow) openinfowindow.close();
+		infowindow.open(map, this);
+		openinfowindow = infowindow;
+	});
 }
 
 function loadUserInputMap() {
@@ -32,47 +74,14 @@ function loadUserInputMap() {
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
+	markers = new Array(results.length);
+	places = new Array(results.length);
     for (var i = 0; i < results.length; i++) {
-      let place = results[i];
-      let marker = createMarker(results[i]);
-      addResultToList(results[i], marker);
+      place = results[i];
+      marker = createMarker(results[i]);
+      markers[i] = marker;
+      places[i] = place;
+      addResultToList(results[i]);
     }
   }
-}
-
-
-// to do: on button click for list: open marker
-
-// add results to list group
-function addResultToList(result, marker) {
-	let resultlist = document.getElementById("resultslist");
-	resultlist.innerHTML += 
-		'<button class="list-group-item" id="' + result.id + '">' +  result.name + '</button>';
-//	let elem = document.getElementById(result.id);
-//	elem.addEventListener("click", function() {
-//		console.log("asdjkalsda");
-//	});
-}
-
-function openMarker() {
-//	alert("hi");
-}
-
-function createMarker(place) {
-	var marker = new google.maps.Marker({
-		map: map,
-		title: place.name,
-		position: place.geometry.location
-	});
-	setMarkerListener(place, marker);
-	return marker;
-}
-
-function setMarkerListener(place, marker) {
-	let infowindow = new google.maps.InfoWindow();
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent('<div><strong>' + place.name + '<strong><br>' +
-				place.formatted_address + '</div>');
-		infowindow.open(map, this);
-	});
 }
